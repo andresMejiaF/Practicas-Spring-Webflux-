349bc3efef0e0b16d536f45d94aee581b68bfad0
andresMejiaF <andressantiagomejiafigueroa@gmail.com>
2025-09-11 19:52:32 -0500

    Implementación de crud reactivo

--- Springwebflux/Módulo 2/Spring-boot-webflux/src/main/java/com/spring/webflux/app/controllers/ProductoController.java
+++ Springwebflux/Módulo 2/Spring-boot-webflux/src/main/java/com/spring/webflux/app/controllers/ProductoController.java
@@ -1,31 +1,33 @@
 package com.spring.webflux.app.controllers;
 
-import com.spring.webflux.app.SpringBootWebfluxApplication;
-import com.spring.webflux.app.models.dao.ProductoDao;
+
 import com.spring.webflux.app.models.documents.Producto;
+import com.spring.webflux.app.models.services.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
 @Controller
 public class ProductoController {
     @Autowired
-    private ProductoDao productoDao;
+    private ProductoService productoService;
 
     private static final Logger log = LoggerFactory.getLogger(ProductoController.class);
 
 
     @GetMapping({"/listar","/"})
-    public  String listar (Model model){
+    public  Mono<String> listar (Model model){
 
-        Flux<Producto> productos = productoDao.findAll()
+        Flux<Producto> productos = productoService.findAllConNombreUpperCase()
                 .map(producto -> {
 
                     producto.setNombre(producto.getNombre().toUpperCase());
@@ -39,18 +41,32 @@ public class ProductoController {
         model.addAttribute("titulo", "Listado de productos");
 
 
-        return "listar";
+        return Mono.just("listar");
     }
+
+    @GetMapping("/form")
+    public Mono<String> crear(Model model){
+
+        model.addAttribute("producto", new Producto() );
+        model.addAttribute("titulo", "formulario de producto");
+        return Mono.just("form");
+    }
+
+
+    @PostMapping("/form")
+    public Mono<String> guardar(Producto producto){
+
+        return productoService.save(producto).doOnNext(p -> {log.info("Producto guardado: " +
+                p.getNombre() +"  ID: " + p.getId());}).thenReturn("redirect:/listar");
+    }
+
+
 
     @GetMapping("/listar-datadriver")
     public  String listarDataDriver(Model model){
 
-        Flux<Producto> productos = productoDao.findAll()
-                .map(producto -> {
-
-                    producto.setNombre(producto.getNombre().toUpperCase());
-                    return producto;
-                }).delayElements(Duration.ofSeconds(1));
+        Flux<Producto> productos = productoService.findAllConNombreUpperCase()
+                .delayElements(Duration.ofSeconds(1));
         productos.subscribe(producto -> log.info(producto.getNombre()));
         model.addAttribute("productos",new ReactiveDataDriverContextVariable(productos,
                 1) ); // al pasar productos por aqui automaticamente se
@@ -62,12 +78,7 @@ public class ProductoController {
     @GetMapping("/listar-full")
     public  String listarFull (Model model){
 
-        Flux<Producto> productos = productoDao.findAll()
-                .map(producto -> {
-
-                    producto.setNombre(producto.getNombre().toUpperCase());
-                    return producto;
-                }).repeat(5000);
+        Flux<Producto> productos = productoService.findAllConNombreUpperCaseRepeat();
 
         model.addAttribute("productos", productos); // al pasar productos por aqui automaticamente se
         // va suscribir
@@ -80,12 +91,7 @@ public class ProductoController {
     @GetMapping("/listar-chunked")
     public  String listarChuncked (Model model){
 
-        Flux<Producto> productos = productoDao.findAll()
-                .map(producto -> {
-
-                    producto.setNombre(producto.getNombre().toUpperCase());
-                    return producto;
-                }).repeat(5000);
+        Flux<Producto> productos = productoService.findAllConNombreUpperCaseRepeat();
 
         model.addAttribute("productos", productos); // al pasar productos por aqui automaticamente se
         // va suscribir
---
+++ Springwebflux/Módulo 2/Spring-boot-webflux/src/main/java/com/spring/webflux/app/models/services/ProductoServiceImpl.java
@@ -0,0 +1,50 @@
+package com.spring.webflux.app.models.services;
+
+import com.spring.webflux.app.models.dao.ProductoDao;
+import com.spring.webflux.app.models.documents.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service //esta notación es un estereotipo de component,
+// indica que esta clase es una fachada, un service en la lógica de negocio
+public class ProductoServiceImpl implements ProductoService{
+
+    @Autowired
+    private ProductoDao dao;
+    @Override
+    public Flux<Producto> findAll() {
+        return dao.findAll();
+    }
+
+    @Override
+    public Flux<Producto> findAllConNombreUpperCase() {
+        return dao.findAll().map(producto -> {
+
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+    }
+
+    @Override
+    public Flux<Producto> findAllConNombreUpperCaseRepeat() {
+        return findAllConNombreUpperCase().repeat(5000);
+    }
+
+    @Override
+    public Mono<Producto> findById(String id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public Mono<Producto> save(Producto producto) {
+        return dao.save(producto);
+    }
+
+    @Override
+    public Mono<Void> delete(Producto producto) {
+        return dao.delete(producto);
+    }
+}
