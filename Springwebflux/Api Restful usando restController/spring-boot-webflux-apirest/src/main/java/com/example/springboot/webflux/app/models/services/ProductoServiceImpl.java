6d5a72cad73fe170e2002a3efb148e82e5615571
andresMejiaF <andressantiagomejiafigueroa@gmail.com>
2025-09-19 16:08:41 -0500

    restcontroller webflux con mongodb

---
+++ Springwebflux/Api Restful usando restController/spring-boot-webflux-apirest/src/main/java/com/example/springboot/webflux/app/models/services/ProductoServiceImpl.java
@@ -0,0 +1,69 @@
+package com.example.springboot.webflux.app.models.services;
+
+import com.example.springboot.webflux.app.models.dao.CategoriaDao;
+import com.example.springboot.webflux.app.models.dao.ProductoDao;
+import com.example.springboot.webflux.app.models.documents.Categoria;
+import com.example.springboot.webflux.app.models.documents.Producto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service //esta notación es un estereotipo de component,
+// indica que esta clase es una fachada, un service en la lógica de negocio
+public class ProductoServiceImpl implements ProductoService{
+
+    @Autowired
+    private ProductoDao dao;
+
+    @Autowired
+    private CategoriaDao categoriaDao;
+    @Override
+    public Flux<Producto> findAll() {
+        return dao.findAll();
+    }
+
+    @Override
+    public Flux<Producto> findAllConNombreUpperCase() {
+        return dao.findAll().map(producto -> {
+
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+    }
+
+    @Override
+    public Flux<Producto> findAllConNombreUpperCaseRepeat() {
+        return findAllConNombreUpperCase().repeat(5000);
+    }
+
+    @Override
+    public Mono<Producto> findById(String id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public Mono<Producto> save(Producto producto) {
+        return dao.save(producto);
+    }
+
+    @Override
+    public Mono<Void> delete(Producto producto) {
+        return dao.delete(producto);
+    }
+
+    @Override
+    public Flux<Categoria> findAllCategorie() {
+        return categoriaDao.findAll();
+    }
+
+    @Override
+    public Mono<Categoria> findCategoriaById(String id) {
+        return categoriaDao.findById(id);
+    }
+
+    @Override
+    public Mono<Categoria> saveCategoria(Categoria categoria) {
+        return categoriaDao.save(categoria);
+    }
+}
