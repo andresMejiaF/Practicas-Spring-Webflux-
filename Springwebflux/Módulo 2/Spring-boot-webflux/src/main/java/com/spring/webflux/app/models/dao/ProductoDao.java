a449cdd14e5421a219f6b98770faef8ffba5d8c4
andresMejiaF <andressantiagomejiafigueroa@gmail.com>
2025-09-23 11:51:05 -0500

    ajustes con handler

--- Springwebflux/Módulo 2/Spring-boot-webflux/src/main/java/com/spring/webflux/app/models/dao/ProductoDao.java
+++
@@ -1,9 +0,0 @@
-package com.spring.webflux.app.models.dao;
-
-import com.spring.webflux.app.models.documents.Producto;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface ProductoDao  extends ReactiveMongoRepository<Producto, String> {
-
-
-}
