2ff4275bee7324029e0e4df3287890b5cadf23a1
andresMejiaF <andressantiagomejiafigueroa@gmail.com>
2025-09-25 20:42:40 -0500

    Nuevo microservicio cliente

---
+++ Springwebflux/Api Restful usando restController/spring-boot-webflux-client/src/main/java/com/example/springboot/webflux/client/app/models/Categoria.java
@@ -0,0 +1,34 @@
+package com.example.springboot.webflux.client.app.models;
+
+
+
+public class Categoria {
+
+    private String id;
+    private String nombre;
+
+    public Categoria() {
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+}
